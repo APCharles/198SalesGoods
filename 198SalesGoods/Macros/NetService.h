eba383412a46c465404cb47d6757d1d2bd5825f8
小安的git <1983653055@qq.com>
2017-08-15 10:32:23 +0800

    kaia

---
+++ 198SalesGoods/Macros/NetService.h
@@ -0,0 +1,16 @@
+
+#import <Foundation/Foundation.h>
+
+@interface NetService : NSObject
+
++ (void)serviceWithPostURL:(NSString *)url
+                       params:(NSDictionary *)param
+                     success:(void (^)(id responseObject))successBlock
+                      failure:(void (^)(NSError *error))failureBlock;
+
++ (void)serviceWithGetURL:(NSString *)url
+                       params:(NSDictionary *)param
+                      success:(void (^)(id responseObject))successBlock
+                      failure:(void (^)(NSError *error))failureBlock;
+
+@end
---
+++ 198SalesGoods/Macros/NetService.m
@@ -0,0 +1,48 @@
+
+#import "NetService.h"
+#import "AFNetworking.h"
+
+@implementation NetService
+
++ (void)serviceWithPostURL:(NSString *)url
+                    params:(NSDictionary *)param
+                   success:(void (^)(id responseObject))successBlock
+                   failure:(void (^)(NSError *error))failureBlock {
+
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+//    NSString *url = [Login_URL stringByAppendingFormat:@"%@", methedName];
+    [manager POST:url parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        successBlock(responseObject);
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        failureBlock(error);
+        
+    }];
+    
+}
+
++ (void)serviceWithGetURL:(NSString *)url
+                   params:(NSDictionary *)param
+                  success:(void (^)(id responseObject))successBlock
+                  failure:(void (^)(NSError *error))failureBlock{
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+        //    NSString *url = [Login_URL stringByAppendingFormat:@"%@", methedName];
+    [manager GET:url parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+        
+        
+    }] ;
+    
+}
+
+
+
+
+
+
+@end
