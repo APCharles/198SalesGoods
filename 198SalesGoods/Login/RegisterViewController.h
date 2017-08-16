c79899803c1502969730d768023d9bec9200ba08
dkxtx <278028476@qq.com>
2017-08-16 17:51:47 +0800

    no message

--- 198SalesGoods/Login/RegisterViewController.h
+++ 198SalesGoods/Login/RegisterViewController.h
@@ -8,6 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RegisterViewController : UIViewController
+@interface RegisterViewController : UIViewController<UITextFieldDelegate>
+{
+    UIScrollView *_mainScrollView;
+    
+    UIView *_mobileView;
+    UITextField *_mobileTextField;
 
+    UIView *_verifyView;
+    UITextField *_verifyTextField;
+    UIButton *_verifyBtn;
+    
+    UIView *_passwordView;
+    UITextField *_passwordTextField;
+    
+    UIView *_nameView;
+    UITextField *_nameTextField;
+    
+    UIView *_addressView;
+    UITextField *_addressTextField;
+    
+    UIView *_pidView;
+    UITextField *_pidTextField;
+}
 @end
