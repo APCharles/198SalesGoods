6c0782fad9bacca1605598e77831ee92edf0f3ce
dkxtx <278028476@qq.com>
2017-08-18 17:01:18 +0800

    no message

---
+++ 198SalesGoods/Goods/GoodsDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  GoodsDetailViewController.h
+//  198SalesGoods
+//
+//  Created by yidianjicai-iOS on 17/8/18.
+//  Copyright © 2017年 AP. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface GoodsDetailViewController : BaseViewController
+
+@end
